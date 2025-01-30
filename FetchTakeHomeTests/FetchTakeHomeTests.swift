1f99438539725eed86871279f668b6e6f7455332
Edgar Barocio <edgar.barocio@gmail.com>
2025-01-30 16:03:30 -0500

    Initial commit

---
+++ FetchTakeHomeTests/FetchTakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeTests.swift
+//  FetchTakeHomeTests
+//
+//  Created by Edgar Barocio on 1/26/25.
+//
+
+import Testing
+@testable import FetchTakeHome
+
+struct FetchTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
